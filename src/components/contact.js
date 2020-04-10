26976d01c70e739faeee110ea79f3d1ddbc4fe90
Andrew Jessen-Tyler <andrewtyl.pro@gmail.com>
2020-04-10 00:38:53 -0600

    contact css done. Services CSS quick touchup.

--- src/components/contact.js
+++ src/components/contact.js
@@ -8,13 +8,14 @@ class Contact extends React.Component {
                 <div id="contact-holder">
                     <ul>
                         <li>
-                            <h4><a href="emailto:ajessen@ajessen.com">Email</a></h4>
+                            <p><a href="emailto:ajessen@ajessen.com">Email: ajessen@ajessen.com</a></p>
                         </li>
                         <li>
-                            <img classname="contact-icons" id="linkedIn-contact-icon" src={require("../assets/LI-Logo.png")} alt="LinkedIn Icon" href="https://www.linkedin.com/in/andrew-j-017b4b9a/" />
-                        </li><li>
-                            <img classname="contact-icons" id="github-contact-logo" src={require("../assets/GitHub_Logo.png")} alt="Github Icon" href="https://github.com/andrewtyl" />
-                            <img classname="contact-icons" id="github-contact-icon" src={require("../assets/GitHub-Mark-120px-plus.png")} alt="Github Icon" href="https://github.com/andrewtyl" />
+                            <img className="contact-icons" id="linkedIn-contact-icon" src={require("../assets/LI-Logo.png")} alt="LinkedIn Icon" href="https://www.linkedin.com/in/andrew-j-017b4b9a/" />
+                        </li><li><div id="github-contact-holder">
+                            <img className="contact-icons" id="github-contact-logo" src={require("../assets/GitHub_Logo.png")} alt="Github Icon" href="https://github.com/andrewtyl" />
+                            <img className="contact-icons" id="github-contact-icon" src={require("../assets/GitHub-Mark-120px-plus.png")} alt="Github Icon" href="https://github.com/andrewtyl" />
+                        </div>
                         </li>
                     </ul>
                 </div>
